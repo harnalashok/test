fba4ede218a84c1ebde3b90426ac8466a89a179d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-08-20 11:38:50 +0530

    Update n8n_reset.sh

--- n8n_reset.sh
+++ n8n_reset.sh
@@ -29,6 +29,8 @@ echo "echo '   Last Name:   harnal'"                       >> /home/$USER/reset_
 echo "echo '   password:   Ashok@12345'"                   >> /home/$USER/reset_n8n.sh
 echo "echo '==**====**====='"                              >> /home/$USER/reset_n8n.sh
 
+password="ashok"
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
 # n8n start script for Ubuntu
 echo '#!/bin/bash'                                                                                                        > /home/$USER/start_n8n.sh
 echo " "                                                                                                                  >> /home/$USER/start_n8n.sh
@@ -40,8 +42,7 @@ echo "echo '4. Delete all workflows as:  sudo rm -rf .n8n/ '"                   
 echo "echo '5. Reset n8n login:          ./reset_n8n.sh'"                                                                 >> /home/$USER/start_n8n.sh
 echo "echo '6. Memory usage: Use \"top -u $USER\" OR \"free -g \" '"                                                      >>  /home/$USER/start_n8n.sh
 echo "sleep 4"                                                                                                             >> /home/$USER/start_n8n.sh
-#echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL=\"http://${IP_ADDRESS}:5678/\" -e NODE_OPTIONS=\"--max-old-space-size=4096\" --network host -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
-echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL="http://192.240.4.114:5678/" -e NODE_OPTIONS="--max-old-space-size=4096" --network host -v /home/ashok/.n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
+echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL=\"http://$IP_ADDRESS:5678/\" -e NODE_OPTIONS=\"--max-old-space-size=4096\" --network host -v /home/ashok/.n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
 echo "echo '  '"                                              >> /home/$USER/start_n8n.sh
 echo "echo '======NOTE==='"                                   >> /home/$USER/start_n8n.sh
 echo "echo 'if n8n does not get started, issue command:'"     >> /home/$USER/start_n8n.sh
@@ -49,8 +50,36 @@ echo "echo '     sudo chown -R 1000:1000 /home/$USER/.n8n'"   >> /home/$USER/sta
 echo "echo ' And restart n8n using this very command'"        >> /home/$USER/start_n8n.sh
 echo "echo '======NOTE==='"                                   >> /home/$USER/start_n8n.sh
 echo "echo 'n8n version is'"                                  >> /home/$USER/start_n8n.sh
-echo "docker exec -it n8n n8n --version"  					  >> /home/$USER/start_n8n.sh
+echo "docker exec -it n8n n8n --version"  		      >> /home/$USER/start_n8n.sh
 echo "echo \"\"\${IP_ADDRESS}\"\""                            >> /home/$USER/start_n8n.sh
+echo "sleep 3"                                                >> /home/$USER/start_n8n.sh
+echo "netstat -aunt | grep 5678"                              >> /home/$USER/start_n8n.sh	
+
+# Hard Reset n8n password
+echo '#!/bin/bash'                                          > /home/$USER/hard_reset_n8n.sh
+echo " "                                                   >> /home/$USER/hard_reset_n8n.sh
+echo "echo 'Stopping n8n, if started'"                     >> /home/$USER/hard_reset_n8n.sh
+echo "docker stop n8n"                                     >> /home/$USER/hard_reset_n8n.sh
+echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+echo "sudo rm -rf /home/$USER/.n8n"			   >> /home/$USER/hard_reset_n8n.sh
+echo "echo 'Restarting n8n'"                               >> /home/$USER/hard_reset_n8n.sh
+echo "bash start_n8n.sh"                                   >> /home/$USER/hard_reset_n8n.sh
+echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+echo "sudo chown -R 1000:1000 /home/$USER/.n8n"            >> /home/$USER/hard_reset_n8n.sh
+echo "echo 'Done...'"                                      >> /home/$USER/hard_reset_n8n.sh
+echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+echo "echo 'Starting n8n again...'"                        >> /home/$USER/hard_reset_n8n.sh
+echo "bash start_n8n.sh"                                   >> /home/$USER/hard_reset_n8n.sh
+echo "echo '==**====**====='"                              >> /home/$USER/hard_reset_n8n.sh
+echo "echo 'For uniformity, keep details as follows:'"     >> /home/$USER/hard_reset_n8n.sh
+echo "echo '   email:       ashok@fsm.ac.in'"              >> /home/$USER/hard_reset_n8n.sh
+echo "echo '   First Name:  ashok'"                        >> /home/$USER/hard_reset_n8n.sh
+echo "echo '   Last Name:   harnal'"                       >> /home/$USER/hard_reset_n8n.sh
+echo "echo '   password:   Ashok@12345'"                   >> /home/$USER/hard_reset_n8n.sh
+echo "echo '==**====**====='"                              >> /home/$USER/hard_reset_n8n.sh
+echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+echo "netstat -aunt | grep 5678"                           >> /home/$USER/hard_reset_n8n.sh
+
 
 # update n8n script
 echo '#!/bin/bash'                                          >  /home/$USER/update_n8n.sh
@@ -63,7 +92,6 @@ echo "echo '==**====**====='"                              >> /home/$USER/update
 echo "docker pull docker.n8n.io/n8nio/n8n "                >> /home/$USER/update_n8n.sh
 echo "echo '   n8n updated'"                               >> /home/$USER/update_n8n.sh
 
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
 # Fallback mechanism if hostname -I returns empty (common on macOS or minimal Linux)
 if [ -z "$IP_ADDRESS" ]; then
 	IP_ADDRESS=$(ip route get 1 // | awk '{print $7}')
