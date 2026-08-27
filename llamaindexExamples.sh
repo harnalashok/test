dc71de8db82c9b2ab79b3bd71881e9b4217f6a60
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-08-27 11:36:48 +0530

    Update llamaindexExamples.sh

--- llamaindexExamples.sh
+++ llamaindexExamples.sh
@@ -27,3 +27,26 @@ cp /home/$USER/Documents/llamaindex/revised14042026/*.pdf    /home/$USER/lprojec
 cp /home/$USER/Documents/llamaindex/revised14042026/*.csv    /home/$USER/lprojects
 cp /home/$USER/Documents/llamaindex/revised14042026/*.ipynb  /home/$USER/lprojects
 cp /home/$USER/Documents/llamaindex/data/md_files/*.md       /home/$USER/lprojects/md_files
+
+echo "Populating lprojects"
+cd /home/$USER
+
+rm -rf /home/$USER/lprojects
+mkdir /home/$USER/lprojects
+mkdir /home/$USER/lprojects/txt_files
+mkdir /home/$USER/lprojects/md_files
+mkdir /home/$USER/lprojects/md_files1
+mkdir /home/$USER/lprojects/pg
+mkdir /home/$USER/lprojects/pdf
+mkdir /home/$USER/lprojects/csv
+
+cp /home/$USER/Documents/llamaindex/data/*.pdf                   /home/$USER/lprojects/pdf
+cp /home/$USER/Documents/llamaindex/data/*.csv                   /home/$USER/lprojects/csv
+cp -r /home/$USER/Documents/llamaindex/revised14042026/txt_files/*  /home/$USER/lprojects/txt_files/
+cp /home/$USER/Documents/llamaindex/revised14042026/*            /home/$USER/lprojects/
+cp /home/$USER/Documents/llamaindex/data/customers-100.csv	     /home/$USER/lprojects/
+cp /home/$USER/Documents/llamaindex/data/md_files/*.md           /home/$USER/lprojects/md_files/
+cp /home/$USER/Documents/llamaindex/postgresql/*.md              /home/$USER/lprojects/md_files1/
+cp /home/$USER/Documents/llamaindex/postgresql/*.ipynb           /home/$USER/lprojects/pg
+cp /home/$USER/Documents/llamaindex/postgresql_SQL/*.py          /home/$USER/lprojects/pg 
+echo "Done......"
