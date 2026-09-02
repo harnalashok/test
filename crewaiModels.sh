15361bfca943a46e72bb8d1e7c5087c26c7e2a1d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-02 17:50:20 +0530

    Create script to install crewai models
    This script installs crewai models by setting up the necessary directories, initializing a git repository, and pulling the required files from a remote repository.

---
+++ crewaiModels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# wget -c https://raw.githubusercontent.com/harnalashok/test/refs/heads/main/llamaindexExamples.sh
+# bash llamaindexExamples.sh
+
+
+
+cd ~/   
+echo "  "
+echo "   "
+echo "Installing crewai Models"
+sleep 3
+rm -rf /home/$USER/Documents/crewaiModels
+rm -rf /home/$USER/Documents/crewaiExamples
+
+mkdir -p /home/$USER/Documents/crewaiExamples
+mkdir -p /home/$USER/Documents/crewai_pjt
+
+cd /home/$USER/Documents/crewaiExamples
+git init
+git remote add origin https://github.com/harnalashok/LLMs.git
+git sparse-checkout init --cone
+git sparse-checkout set crewaiModels
+git pull origin main
+find . -maxdepth 1 ! -name "crewaiModels" ! -name "." ! -name ".." -delete
+cd /home/$USER/Documents
+mkdir crewaiModels
+cd crewaiModels
+mv /home/$USER/Documents/crewaiExamples/crewaiModels/* .
+rm -rf /home/$USER/Documents/crewaiExamples
+cd /home/$USER
+cp -r /home/$USER/Documents/crewaiModels/*  /home/$USER/crewai_pjt
