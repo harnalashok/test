d9f27ab66f6e541fcc1d6b2654709c4e0f56db02
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-08 05:10:49 +0530

    Update crewaiModels.sh

--- crewaiModels.sh
+++ crewaiModels.sh
@@ -13,7 +13,7 @@
 
 
 
-##########
+cd /home/$USER
 echo "  "
 echo "------------"                            
 echo " Will install crewai"                     
@@ -21,43 +21,73 @@ echo "----------"
 echo " "
 cd /home/$USER
 sleep 2
+# 1. Switch to your home directory
+cd ~/
+curl -LsSf https://astral.sh/uv/install.sh | sh 
+# 2. Compile to a local temp file (which uv deletes instantly anyway)
+uv pip compile --system -o ~/temp_reqs.txt -q <(cat <<EOF
+crewai
+crewai-tools
+crewai-cli
+langchain
+langchain-cli
+langchain-openai
+langchain-ollama
+langchain-community
+langchain-experimental
+langchain-classic
+yfinance
+llama-index
+llama-index-llms-groq
+llama-index-core
+llama-index-readers-file
+llama-index-embeddings-huggingface
+llama-index-experimental
+pandas
+newsapi-python
+crewai[tools]
+crewai-tools[mcp]
+ddgs
+duckduckgo-search
+EOF
+)
+
+# 3. Clean up the placeholder file
+#rm -f ~/temp_reqs.txt
+
+uv tool install --force crewai
 
 # Our project folder
+rm -rf /home/$USER/crewai_pjt 
 mkdir /home/$USER/crewai_pjt
 # Make it writable by any program
 chmod -R 777 /home/$USER/crewai_pjt
+cd crewai_pjt
+uv init
+uv add crewai crewai-tools
+# # c) Now install crewai and other packages using uv
+uv add crewai crewai-tools crewai-cli langchain langchain-cli
+uv add langchain-openai langchain-ollama langchain-community  
+uv add langchain-experimental langchain-classic yfinance 
+uv add llama-index llama-index-llms-groq llama-index-core
+uv add llama-index-readers-file llama-index-embeddings-huggingface  
+uv add llama-index llama-index-experimental pandas
+uv add 'crewai[tools]'  newsapi-python
+uv add 'crewai-tools[mcp]'
+uv add ddgs duckduckgo-search
+cd /home/$USER
 
-# Delete existing env
-rm -rf /home/$USER/crewai_env
-python3.13 -m venv crewai_env
-# b) Activate the env
-source /home/$USER/crewai_env/bin/activate
-
-# c) Now install crewai and other packages using uv
-uv pip install crewai crewai-tools crewai-cli langchain langchain-cli
-uv pip install langchain-openai langchain-ollama langchain-community  
-uv pip install langchain-experimental langchain-classic yfinance 
-uv pip install llama-index llama-index-llms-groq llama-index-core
-uv pip install llama-index-readers-file llama-index-embeddings-huggingface  
-uv pip install llama-index llama-index-experimental pandas
-uv pip install 'crewai[tools]'  newsapi-python
-uv pip install 'crewai-tools[mcp]'
-deactivate
-
-# Create script to activate 'crewai_env' env
+# Create script to activate 'crewai_pjt' env
 echo '#!/bin/bash'                                                         | tee     /home/$USER/activate_crewai_env.sh
 echo "echo 'Execute this file as: source activate_crewai_env.sh' "         | tee -a  /home/$USER/activate_crewai_env.sh
-echo "echo 'source /home/$USER/crewai_env/bin/activate' "                  | tee -a  /home/$USER/activate_crewai_env.sh
+echo "echo 'source /home/$USER/crewai_pjt/.venv/bin/activate' "            | tee -a  /home/$USER/activate_crewai_env.sh
 echo "echo 'Or, as:               . activate_crewai_env.sh' "              | tee -a  /home/$USER/activate_crewai_env.sh
 echo "echo '(Note the change in prompt after activating)' "                | tee -a  /home/$USER/activate_crewai_env.sh
 echo "echo '(To deactivate, just enter the command: deactivate)' "         | tee -a  /home/$USER/activate_crewai_env.sh
-echo "source /home/$USER/crewai_env/bin/activate"                          | tee -a  /home/$USER/activate_crewai_env.sh
+echo "source /home/$USER/crewai_pjt/.venv/bin/activate"                    | tee -a  /home/$USER/activate_crewai_env.sh
 echo "cd /home/$USER/crewai_pjt"                                           | tee -a  /home/$USER/activate_crewai_env.sh
 
-
 ###########
-
-
 
 cd ~/   
 echo "  "
@@ -86,3 +116,11 @@ mv /home/$USER/Documents/crewaiExamples/crewaiModels/* .
 rm -rf /home/$USER/Documents/crewaiExamples
 cd /home/$USER
 cp -r /home/$USER/Documents/crewaiModels/*  /home/$USER/crewai_pjt
+mkdir /home/ashok/crewai_pjt/servers
+cp /home/ashok/crewai_pjt/mcp_servers/'maths_stdio server'/math_stdio_server.py /home/ashok/crewai_pjt/servers/
+cp /home/ashok/crewai_pjt/mcp_servers/'maths_stdio server'/math_stdio_client.py /home/ashok/crewai_pjt/
+
+
+
+
+
