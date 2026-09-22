7b76f61c9de59f75f59e017da6eb8c8515dc86f1
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-22 07:01:12 +0530

    Update mcpServers.sh

--- mcpServers.sh
+++ mcpServers.sh
@@ -2,11 +2,19 @@
 
 # LAst amended: 22nd Sep, 2026
 
+## =========
+# Replace existing mcp_servers folder in WSL ubuntu 
+#  cd ~/
+#  wget -Nc https://raw.githubusercontent.com/harnalashok/test/refs/heads/main/mcpServers.sh
+#  chmod +x *.sh
+#  bash mcpServers.sh
+## =========
+
 cd ~/
 wget -Nc https://github.com/harnalashok/LLMs/blob/main/crewaiModels/mcpServer.zip?raw=true
 mv mcpServer.zip\?raw\=true  mcpServer.zip
-rm -rf /home/ashok/crewai_pjt/mcp_servers/*
-mv mcpServer.zip /home/ashok/crewai_pjt/mcp_servers/
-cd /home/ashok/crewai_pjt/mcp_servers/
+rm -rf /home/$USER/crewai_pjt/mcp_servers/*
+mv mcpServer.zip /home/$USER/crewai_pjt/mcp_servers/
+cd  /home/$USER/crewai_pjt/mcp_servers/
 unzip mcpServer.zip
 cd ~/
