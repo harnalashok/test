227349de4373b4fa864b87e48e26d4fea32762de
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-24 14:41:35 +0530

    Update mcpServers.sh

--- mcpServers.sh
+++ mcpServers.sh
@@ -22,12 +22,15 @@ cd  /home/$USER/crewai_pjt/mcp_servers/
 unzip mcpServers.zip
 cd ~/
 
+read -p "Press [Enter] to continue..."
+sleep 5
 cd ~/
  . activate_crewai_env.sh
 uv add llama-index-llms-ollama
 uv add llama-index-embeddings-ollama
 uv add "mcp[cli]" pandas-ta alpaca-py
 uv add ollama
+uv add fastmcp
 cd /home/ashok/crewai_pjt/python_approach/CR1_files
 mv 7a.sports_rag_analyst_intelli.py 7c.sports_rag_analyst_intelli.py
 wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/crewaiModels/python_approach/CR1_files/7a.sports_rag_analyst_intelli.py
